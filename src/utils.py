2bf91daf7f0f29e39faf9ce8aa9be710c0907f23
Xinyue Wang <xinwang@guidewire.com>
2020-03-19 18:02:52 +0000

    Fix deprecate errors
    Quite a few code is out-of-date therefore doesn't work anymore.
    So I amend it to a status that is working, however, it is using
    v1 tf apis therefore, proper fix is necessary through using
    new interfaces in tf v2.

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import scipy.misc, numpy as np, os, sys
+import imageio
 
 def save_img(out_path, img):
     img = np.clip(img, 0, 255).astype(np.uint8)
-    scipy.misc.imsave(out_path, img)
+    imageio.imsave(out_path, img)
 
 def scale_img(style_path, style_scale):
     scale = float(style_scale)
@@ -13,7 +14,7 @@ def scale_img(style_path, style_scale):
     return style_target
 
 def get_img(src, img_size=False):
-   img = scipy.misc.imread(src, mode='RGB') # misc.imresize(, (256, 256, 3))
+   img = imageio.imread(src, pilmode='RGB') # misc.imresize(, (256, 256, 3))
    if not (len(img.shape) == 3 and img.shape[2] == 3):
        img = np.dstack((img,img,img))
    if img_size != False:
